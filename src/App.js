aecea7e14abec9eb06e92e14dc05e6d51b0dcb32
hackurity01 <hackurity01@gmail.com>
2020-05-14 15:47:32 +0900

    1-9 Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {Suspense, lazy} from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+// import ListPage from './pages/ListPage/index'
+// import ViewPage from './pages/ViewPage/index'
+
+const ListPage = lazy(() => import('./pages/ListPage/index'))
+const ViewPage = lazy(() => import('./pages/ViewPage/index'))
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={ListPage} exact />
-        <Route path="/view/:id" component={ViewPage} exact />
-      </Switch>
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <Switch>
+          <Route path="/" component={ListPage} exact />
+          <Route path="/view/:id" component={ViewPage} exact />
+        </Switch>
+      </Suspense>
     </div>
   )
 }
