ef3382fb9a793b71be9b0a668ece97872b05f304
hackurity01 <hackurity01@gmail.com>
2022-07-10 20:11:59 +0900

    1-5 병목 코드 최적화

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -17,22 +17,8 @@ function getParametersForUnsplash({width, height, quality, format}) {
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
 function removeSpecialCharacter(str) {
-  const removeCharacters = ['#', '_', '*', '~', '&', ';', '!', '[', ']', '`', '>', '\n', '=', '-']
-  let _str = str
-  let i = 0,
-    j = 0
-
-  for (i = 0; i < removeCharacters.length; i++) {
-    j = 0
-    while (j < _str.length) {
-      if (_str[j] === removeCharacters[i]) {
-        _str = _str.substring(0, j).concat(_str.substring(j + 1))
-        continue
-      }
-      j++
-    }
-  }
-
+  let _str = str.substring(0, 300)
+  _str = str.replace(/[#_*~&;![\]`>\n=\->]/g, '')
   return _str
 }
 
